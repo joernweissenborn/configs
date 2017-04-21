a4225e3198431ab645c674e45162433238d504c5
Joern Weissenborn <joern@localhost.localdomain>
2017-04-21 14:39:40 -0400

    updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,27 @@
 
 cd ~
 
-ln -s configs/zsh.rc .zsh.rc
-ln -s configs/vim.rc .vim.rc
+echo "Installing stuff"
+
+sudo dnf install vim git python3-ipython tmux zsh htop util-linux-user redhat-rpm-config python3-devel
+sudo pip3 install thefuck
+
+echo "Doing Oh My zsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+echo "creating symlinks"
+
+ln -s configs/zshrc .zshrc
+ln -s configs/vimrc .vimrc
 ln -s configs/tmux.conf .tmux.conf
+
+
+
+echo "Installing Vundle"
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c PluginInstall -c quitall
 
 echo "done"
--- vimrc
+++ vimrc
@@ -83,6 +83,23 @@ call vundle#end()            " required
 
 filetype plugin indent on    " required
 
+" Putty Colors
+
+if &term =~ "xterm"
+  " 256 colors
+  let &t_Co = 256
+  "     " restore screen after quitting
+  let &t_ti = "\<Esc>7\<Esc>[r\<Esc>[?47h"
+  let &t_te = "\<Esc>[?47l\<Esc>8"
+  if has("terminfo")
+    let &t_Sf = "\<Esc>[3%p1%dm"
+    let &t_Sb = "\<Esc>[4%p1%dm"
+  else
+    let &t_Sf = "\<Esc>[3%dm"
+    let &t_Sb = "\<Esc>[4%dm"
+  endif
+endif
+
 set number
 set mouse=a
 colorscheme molokai
