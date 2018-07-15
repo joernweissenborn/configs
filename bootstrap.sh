04c9e0180bd4dc999eb67a1546c3113d9bf830b0
Joern Weissenborn <joern.weissenborn@gmail.com>
2018-07-15 14:29:53 +0200

    refactor

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,11 @@
 #!/bin/sh
 
-cd ~
-
-echo "creating symlinks"
-
-ln -s configs/zshrc .zshrc
-ln -s configs/vimrc .vimrc
-ln -s configs/tmux.conf .tmux.conf
-
 echo "Installing stuff"
 
 sudo dnf update -y
-sudo dnf install -y vim git python3-ipython tmux zsh htop util-linux-user redhat-rpm-config python3-devel
+sudo dnf install -y git python3-pip python3-ipython htop util-linux-user redhat-rpm-config python3-devel
 sudo pip3 install -y thefuck
 
 echo "creating symlinks"
 
-ln -s configs/zshrc .zshrc
-ln -s configs/vimrc .vimrc
 ln -s configs/tmux.conf .tmux.conf
-
-echo "Doing Oh My zsh"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-
-echo "Installing Vundle"
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim -c PluginInstall -c quitall
-
-echo "done"
