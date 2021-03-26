aff16c4f60d726a4826480148ecd8562b533efe6
Joern Weissenborn <joern.weissenborn@gmail.com>
2021-03-26 12:39:11 +0100

    Added git config to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,10 @@ echo "Installing stuff"
 sudo dnf update -y
 sudo dnf install -y git python3-pip python3-ipython flake8 guake htop util-linux-user redhat-rpm-config python3-devel gcc-c++ make
 sudo pip3 install thefuck
+
+echo "configuring git"
+
+git config --global user.name "Joern Weissenborn"
+git config --global user.email joern.weissenborn@gmail.com
+git config --global user.signingkey joern.weissenborn@gmail.com
+git config --global commit.gpgSign true
